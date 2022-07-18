03c05ee7b7f08cf540a855f7df84771921cef6b0
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-07-18 09:02:23 -0300

    Pagina Home e configurando o Prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +27,36 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+        <div className={styles.posts}>
+          <Link href="/">
+            <a className={styles.post}>
+              <strong>Algum titulo</strong>
+              <p>aasfasfasf asfasfasf </p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  01/01/2020
+                </li>
+                <li>
+                  <FiUser />
+                  Armando Marioto
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <button type="button" className="">
+            Carregar mais posts
+          </button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
